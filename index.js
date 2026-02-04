91f3c7721e79c2842deaede08af485cd089e675f
apram-kaur <apramkaur_bhatia@srmap.edu.in>
2026-02-04 23:48:16 +0530

    Add files via upload
    Add user APIs and Postman-tested routes
    Update index.js with signup, login, and users routes

--- index.js
+++ index.js
@@ -2,30 +2,86 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
+const users = [];
+
 
 // Test route
 app.get("/", (req, res) => {
-  res.send("Splitwise backend running 🚀");
+  res.send("Server Restarted");
 });
 app.post("/signup", (req, res) => {
   const { name, email, password } = req.body;
 
-  // basic validation
+  // validation
   if (!name || !email || !password) {
     return res.status(400).json({
       error: "All fields are required",
     });
   }
 
+  // duplicate email check
+  const existingUser = users.find(user => user.email === email);
+
+  if (existingUser) {
+    return res.status(409).json({
+      error: "Email already registered"
+    });
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name,
+    email,
+    password,
+  };
+
+  users.push(newUser);
+
   res.status(201).json({
     message: "User signup successful",
     user: {
-      name,
-      email,
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
     },
   });
 });
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Email and password required"
+    });
+  }
+
+  const user = users.find(user => user.email === email);
+
+  if (!user) {
+    return res.status(404).json({
+      error: "User not found"
+    });
+  }
+
+  if (user.password !== password) {
+    return res.status(401).json({
+      error: "Invalid password"
+    });
+  }
+
+  res.status(200).json({
+    message: "Login successful",
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    }
+  });
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
